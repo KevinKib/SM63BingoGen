fae4c4cea6591a4312ab18285401376a852352cd
Sekanor <sekanor21@gmail.com>
2018-06-24 12:29:01 +0200

    Adding packages

--- SM63BingoGen/src/sm63bingogen/IHM/Message.java
+++ SM63BingoGen/src/sm63bingogen/IHM/Message.java
@@ -13,9 +13,15 @@ import java.util.HashMap;
  */
 public class Message {
     
+    // Unique instance of the Message class.
     private static Message instance;
+    
+    // List of messages, which link a string to another string
     private HashMap<String, String> messageList;
     
+    /**
+     * Constructor of the Message class.
+     */
     private Message() {
         this.messageList = new HashMap<>();
         
@@ -39,14 +45,28 @@ public class Message {
         addMessage("b_copyClipboard_valid","Copied !");
     }
     
+    /**
+     * Adds a message to the messageList HashMap.
+     * @param title Title&ID of the message.
+     * @param message String linked to the title.
+     */
     private void addMessage(String title, String message) {
         this.messageList.put(title, message);
     }
     
+    /**
+     * Returns a certain message after having entered its title.
+     * @param title Title&ID of the message.
+     * @return The string linked to the title.
+     */
     public String get(String title) {
         return this.messageList.get(title);
     }
     
+    /**
+     * Returns the unique instance of the Message class.
+     * @return The unique instance of the Message class.
+     */
     public static Message getInstance() {
         if (instance == null) {
             instance = new Message();
